f1d88df6b47593687ae1a1ba57120682e17ca5f5
KevinYakha <kevin@yakha.eu>
2024-05-07 10:55:18 +0200

    Added circle shape

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -8,9 +8,14 @@ class Program
         Shape greenRectangle = new Rectangle(new GreenRectangle(), 100, 200, 32, 32);
         Shape yellowRectangle = new Rectangle(new YellowRectangle(), 150, 250, 24, 24);
 
+        Shape greenCircle = new Circle(new GreenRectangle(), 200, 250, 16);
+
         // Drawing the rectangle.
         greenRectangle.Draw();
         yellowRectangle.Draw();
+
+        Console.WriteLine("\ngreenCircle Draw():");
+        greenCircle.Draw();
 
         // Prevents the console window from closing immediately.
         Console.ReadKey();
